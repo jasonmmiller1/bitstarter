4e27b25d08dafe0c90d05a733c464a21b65ef3eb
jasonmmiller <jasonmmiller@gmail.com>
2013-07-10 03:52:15 +0000

    web.js update

--- web.js
+++ web.js
@@ -1,3 +1,7 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
 fs = require('fs')
 fs.readFile('index.html', 'utf8', function (err,data) {
   if (err) {
@@ -5,3 +9,8 @@ fs.readFile('index.html', 'utf8', function (err,data) {
   }
   console.log(data);
 });
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+}};
